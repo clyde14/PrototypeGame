4d9eb9ddbb67d974a6d0e539fdd9a289f90c0c16
Dane <winiesdl@miamioh.edu>
2020-02-01 17:18:10 -0500

    Added the ability to move certain platforms! Also removed drag and drop asset

---
+++ Assets/Scripts/MovePlatforms.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatforms : MonoBehaviour
+{
+    private float startposX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+    public BoxCollider2D toggleBox;
+    // Update is called once per frame
+    void Update()
+    {
+        if(isBeingHeld == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startposX, mousePos.y - startPosY, 0);
+            toggleBox.enabled = false;
+        }
+        else
+        {
+            toggleBox.enabled = true;
+        }
+    }
+    private void OnMouseDown()
+    {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+
+            startposX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            isBeingHeld = true;
+        }
+       
+
+    }
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+}
