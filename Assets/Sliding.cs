5171a6d45930963541d164f38eb627bef883b9f0
yanga3 <yanga2@miamioh.edu>
2020-02-01 15:07:42 -0500

    More Tiles
    Added sliding and dropping tiles

---
+++ Assets/Sliding.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sliding : MonoBehaviour
+{
+
+    public float moveSpeed = 3f;
+    public float moveDistance = 4f;
+    public bool moveRight = true;
+
+    // Update is called once per frame
+    void Update() {
+        if (transform.position.x > moveDistance)
+            moveRight = false;
+        if (transform.position.x < -moveDistance)
+            moveRight = true;
+
+        if (moveRight)
+            transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
+        else
+            transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
+    }
+
+    void OnCollisionEnter2D (Collision2D col) {
+
+        col.transform.parent = this.transform;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+
+        col.transform.parent = null;
+    }
+}
