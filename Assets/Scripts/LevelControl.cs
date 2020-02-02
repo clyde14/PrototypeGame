9ee97c7829d70ed87c39646710c8e28985862b0c
yanga3 <yanga2@miamioh.edu>
2020-02-02 16:24:29 -0500

    Scene transition
    Flag now enables transition between scenes, but music does not continue playing when transitioning from level-1 to level-2...

---
+++ Assets/Scripts/LevelControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int index;
+    bool entered = false;
+    //public string levelName;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !entered)
+        {
+            FindObjectOfType<AudioManager>().Play("Victory");
+            entered = true;
+            StartCoroutine(WaitToLoad());
+            //SceneManager.LoadScene(levelName);
+        }
+
+        
+    }
+
+    IEnumerator WaitToLoad()
+    {
+        yield return new WaitForSeconds(2.8f);
+        SceneManager.LoadScene(index);
+    }
+}
